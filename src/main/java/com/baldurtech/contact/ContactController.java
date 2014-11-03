595106e88c31d18bd02d71ae269aa5cbcc06e115
shihang <study1228.sh@gmail.com>
2014-11-03 15:29:39 +0800

    在页面中添加list属性

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -1,5 +1,8 @@
 package com.baldurtech.contact;
 
+import java.util.List;
+import java.util.ArrayList;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("contact")
 public class ContactController {
     @RequestMapping(value = "list")
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("contactList", new ArrayList());
         return "contact/list";
     }
 }
