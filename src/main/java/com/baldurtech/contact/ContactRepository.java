108ec42d4807db3899c562cb643edb1a9eaa6632
shihang <study1228.sh@gmail.com>
2014-11-03 16:48:00 +0800

    添加数据库的连接并完成findA的功能

--- src/main/java/com/baldurtech/contact/ContactRepository.java
+++ src/main/java/com/baldurtech/contact/ContactRepository.java
@@ -3,18 +3,20 @@ package com.baldurtech.contact;
 import java.util.List;
 import java.util.ArrayList;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.*;
+
+@Transactional
 @Repository
 public class ContactRepository {
+    @PersistenceContext
+    EntityManager entityManager;
     public List<Contact> findAll() {
-        List<Contact> contactList = new ArrayList<Contact>();
-        Contact contact = new Contact();
-        contact.setName("ShiHang");
-        contact.setMobile("15235432994");
-        contact.setVpmn("652994");
-        contact.setHomeAddress("TaiYuan");
-        
-        contactList.add(contact);
-        return contactList;
+        try {        
+            return entityManager.createNamedQuery(Contact.FIND_ALL, Contact.class).getResultList();
+        } catch(PersistenceException pe) {
+            return null;
+        }
     }
 }
