fbf0b853dec8f35e8d9f1bc1ecacc7d27d080bbd
baijiayong <jiayongbai@gmail.com>
2014-11-06 23:27:20 +0800

    给页面传的属性contactList设置值

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -14,7 +14,24 @@ import org.springframework.ui.Model;
 public class ContactController {
     @RequestMapping(value = "list", method = RequestMethod.GET)
     public String list(Model model) {
-        model.addAttribute("contactList", new ArrayList<>());
+        List<Contact> contactList = new ArrayList<Contact>();
+        
+        Contact contact1 = new Contact();
+        contact1.setName("Xiao Bai");
+        contact1.setMobile("18233333333");
+        contact1.setVpmn("62222");
+        contact1.setHomeAddress("TaiYuan");
+        
+        Contact contact2 = new Contact();
+        contact2.setName("Shi Hang");
+        contact2.setMobile("18222222222");
+        contact2.setVpmn("63333");
+        contact2.setHomeAddress("TaiYuan");
+        
+        contactList.add(contact1);
+        contactList.add(contact2);
+        
+        model.addAttribute("contactList", contactList);
         return "contact/list";
     }
 }
