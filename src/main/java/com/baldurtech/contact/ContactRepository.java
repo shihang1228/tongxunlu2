e24077a8d38bf881849065c0effe5d1b068b3723
shihang <study1228.sh@gmail.com>
2014-11-10 11:25:21 +0800

    在contactService 中调用contactRepository中的findAll方法

---
+++ src/main/java/com/baldurtech/contact/ContactRepository.java
@@ -0,0 +1,23 @@
+package com.baldurtech.contact;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ContactRepository {
+    public List<Contact> findAll() {
+        Contact contact = new Contact();
+        List<Contact> contactList = new ArrayList<Contact>();
+        
+        contact.setName("ShiHang");
+        contact.setMobile("15235432994");
+        contact.setEmail("shihang@qq.com");
+        contact.setHomeAddress("TaiYuan");
+        contact.setVpmn("652994");
+        
+        contactList.add(contact);
+        return contactList;
+    }
+}
--- src/main/java/com/baldurtech/contact/ContactService.java
+++ src/main/java/com/baldurtech/contact/ContactService.java
@@ -4,20 +4,18 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class ContactService {
+    ContactRepository contactRepository;
+    
+    @Autowired
+    public ContactService(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
     public List<Contact> findAll() {
-        Contact contact = new Contact();
-        List<Contact> contactList = new ArrayList<Contact>();
-        
-        contact.setName("ShiHang");
-        contact.setMobile("15235432994");
-        contact.setEmail("shihang@qq.com");
-        contact.setHomeAddress("TaiYuan");
-        contact.setVpmn("652994");
-        
-        contactList.add(contact);
-        return contactList;
+        return contactRepository.findAll();
     }
 }
