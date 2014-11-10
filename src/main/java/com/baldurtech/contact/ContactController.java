145da3e9ef7866f2e1b8b4d00b7045ed1a62c187
shihang <study1228.sh@gmail.com>
2014-11-10 11:18:41 +0800

    在Controller中调用service中的findAll方法

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -5,30 +5,24 @@ import java.util.ArrayList;
 
 import org.springframework.ui.Model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("contact")
 public class ContactController {
+    ContactService contactService;
+    
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+    
     @RequestMapping("list")
     public String list(Model model) {
         
-        model.addAttribute("contactList", findAll());
+        model.addAttribute("contactList", contactService.findAll());
         return "contact/list";
     }
-    
-    public List<Contact> findAll() {
-            Contact contact = new Contact();
-            List<Contact> contactList = new ArrayList<Contact>();
-            
-            contact.setName("ShiHang");
-            contact.setMobile("15235432994");
-            contact.setEmail("shihang@qq.com");
-            contact.setHomeAddress("TaiYuan");
-            contact.setVpmn("652994");
-            
-            contactList.add(contact);
-            return contactList;
-        }
 }
