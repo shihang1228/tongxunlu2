f414230ce359df9b29045694b9261245f85d1482
shihang <study1228.sh@gmail.com>
2014-11-14 11:51:43 +0800

    让测试通过，在contactService中调用contactRepository中的findAll方法

---
+++ src/main/java/com/baldurtech/contact/ContactRepository.java
@@ -0,0 +1,29 @@
+package com.baldurtech.contact;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ContactRepository {
+    public List<Contact> findAll() {
+        List<Contact> contactList = new ArrayList<Contact>();
+        Contact contact = new Contact();
+        contact.setName("Shihang");
+        contact.setMobile("15235432994");
+        contact.setVpmn("652994");
+        contact.setHomeAddress("taiyuan");
+        
+        Contact contact2 = new Contact();
+        contact2.setName("Shihang");
+        contact2.setMobile("15235432994");
+        contact2.setVpmn("652994");
+        contact2.setHomeAddress("taiyuan");
+        
+        contactList.add(contact);
+        contactList.add(contact2);
+        
+        return contactList;
+    }
+}
--- src/main/java/com/baldurtech/contact/ContactService.java
+++ src/main/java/com/baldurtech/contact/ContactService.java
@@ -3,26 +3,18 @@ package com.baldurtech.contact;
 import java.util.List;
 import java.util.ArrayList;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class ContactService {
+    ContactRepository contactRepository;
+    
+    @Autowired
+    public ContactService(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
     public List<Contact> findAll() {
-        List<Contact> contactList = new ArrayList<Contact>();
-        Contact contact = new Contact();
-        contact.setName("Shihang");
-        contact.setMobile("15235432994");
-        contact.setVpmn("652994");
-        contact.setHomeAddress("taiyuan");
-        
-        Contact contact2 = new Contact();
-        contact2.setName("Shihang");
-        contact2.setMobile("15235432994");
-        contact2.setVpmn("652994");
-        contact2.setHomeAddress("taiyuan");
-        
-        contactList.add(contact);
-        contactList.add(contact2);
-        
-        return contactList;
+        return contactRepository.findAll();
     }
 }
