d362ca194755dfd6746d001870baf5958208341c
baijiayong <jiayongbai@gmail.com>
2014-11-14 10:28:43 +0800

    添加了失败的集成测试，判断contactController中是否调用了model的addAttribute方法

--- src/test/java/com/baldurtech/contact/ContactControllerUnitTest.java
+++ src/test/java/com/baldurtech/contact/ContactControllerUnitTest.java
@@ -1,12 +1,35 @@
 package com.baldurtech.contact;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
+import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.any;
+
+import org.springframework.ui.Model;
+
 public class ContactControllerUnitTest {
-    ContactController contactController = new ContactController();
+    @InjectMocks
+    ContactController contactController;
+    
+    @Mock
+    Model model;
+    
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
     @Test
     public void 在list方法中当URL为contact_list时返回contact_list() {
-        assertEquals("contact/list", contactController.list());
+        assertEquals("contact/list", contactController.list(model));
+        verify(model).addAttribute("contactList", new ArrayList<Contact>());
     }
 }
