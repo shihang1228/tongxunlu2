045e840ba458c542815ce78a75944e6fa56eff8f
shihang <study1228.sh@gmail.com>
2014-11-14 11:25:21 +0800

    添加了失败的测试：在list方法中当URL为contact_list时返回contact_list

--- src/test/java/com/baldurtech/contact/ContactControllerUnitTest.java
+++ src/test/java/com/baldurtech/contact/ContactControllerUnitTest.java
@@ -11,25 +11,44 @@ import org.mockito.Mock;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 
 import org.springframework.ui.Model;
 
 public class ContactControllerUnitTest {
-    @InjectMocks
-    ContactController contactController;
+    Contact contact;
+    
+    @Mock
+    ContactService contactService;
     
     @Mock
     Model model;
     
+    @InjectMocks
+    ContactController contactController;
+    
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        contact = new Contact();
+        contact.setName("Shihang");
+        contact.setMobile("15235432994");
+        contact.setVpmn("652994");
+        contact.setHomeAddress("taiyuan");
+        contact.setOfficeAddress("taiyuan");
+        contact.setMemo("memo");
+        contact.setJob("HR");
+        contact.setJobLevel(4L);
     }
     
     @Test
     public void 在list方法中当URL为contact_list时返回contact_list() {
+        List<Contact> contactList = new ArrayList<Contact>();
+        when(contactService.findAll()).thenReturn(contactList);
+        
         assertEquals("contact/list", contactController.list(model));
-        verify(model).addAttribute("contactList", new ArrayList<Contact>());
+        verify(contactService).findAll();
+        verify(model).addAttribute("contactList", contactList);
+        
     }
 }
