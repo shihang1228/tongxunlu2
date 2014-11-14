50676da262b535f541a615fea361fd605f35ef17
shihang <study1228.sh@gmail.com>
2014-11-14 14:55:40 +0800

    添加了自动初始化数据的代码

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into contact (name, email, mobile, vpmn, memo, officeAddress, homeAddress, job, jobLevel) values("shihang", "study1228.sh@gmail.com", "15235432994", "652994","memo", "taiyuan", "xiaodian", "HR", 4);
+insert into contact (name, email, mobile, vpmn, memo, officeAddress, homeAddress, job, jobLevel) values("xiaobai", "xiaobai@gmail.com", "18235100872", "60872","memo", "taiyuan", "xiaodian", "HR", 4);
+insert into contact (name, email, mobile, vpmn, memo, officeAddress, homeAddress, job, jobLevel) values("shuangshuang", "shuangshuang@gmail.com", "15234142323", "652323","memo", "taiyuan", "xiaodian", "HR", 4);
+insert into contact (name, email, mobile, vpmn, memo, officeAddress, homeAddress, job, jobLevel) values("renjian", "renjian@gmail.com", "18712343212", "653456","memo", "taiyuan", "xiaodian", "HR", 4);
+insert into contact (name, email, mobile, vpmn, memo, officeAddress, homeAddress, job, jobLevel) values("yufei", "yufei@gmail.com", "15234456788", "651111","memo", "taiyuan", "xiaodian", "HR", 4);
+insert into contact (name, email, mobile, vpmn, memo, officeAddress, homeAddress, job, jobLevel) values("benben", "benben@gmail.com", "1523588766554", "652211","memo", "taiyuan", "xiaodian", "HR", 4);
+insert into contact (name, email, mobile, vpmn, memo, officeAddress, homeAddress, job, jobLevel) values("zhangyu", "zhangyu@gmail.com", "18235888694", "657890","memo", "taiyuan", "xiaodian", "HR", 4);
