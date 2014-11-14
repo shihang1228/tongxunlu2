534a0a584585c8aa64481d94cf73050aff063a7c
shihang <study1228.sh@gmail.com>
2014-11-14 09:19:23 +0800

    添加了一个失败的测试：当URL为contact/list时应该访问list页面

---
+++ src/test/java/com/baldurtech/contact/ContactControllerTest.java
@@ -0,0 +1,15 @@
+package com.baldurtech.contact;
+
+import org.junit.Test;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import com.baldurtech.config.WebAppConfigurationAware;
+
+public class ContactControllerTest extends WebAppConfigurationAware {
+    @Test
+    public void 当URL为contact_list时应该访问list页面() throws Exception{
+        mockMvc.perform(get("/contact/list"))
+               .andExpect(view().name("contact/list"));
+    }
+}
