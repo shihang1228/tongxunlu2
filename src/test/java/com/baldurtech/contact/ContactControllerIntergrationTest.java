23032f2912c7dce5b304d890625239412e574731
baijiayong <jiayongbai@gmail.com>
2014-11-14 10:27:38 +0800

    添加了失败的集成测试:判断attribute是否存在

--- src/test/java/com/baldurtech/contact/ContactControllerIntergrationTest.java
+++ src/test/java/com/baldurtech/contact/ContactControllerIntergrationTest.java
@@ -10,6 +10,7 @@ public class ContactControllerIntergrationTest extends WebAppConfigurationAware 
     @Test
     public void 当URL为contact_list时应该访问list页面() throws Exception{
         mockMvc.perform(get("/contact/list"))
-               .andExpect(view().name("contact/list"));
+               .andExpect(view().name("contact/list"))
+               .andExpect(model().attributeExists("contactList"));
     }
 }
