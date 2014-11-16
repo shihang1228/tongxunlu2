54826900cd448a32dc4dcef28d4a6fe0c2ee503b
shihang <study1228.sh@gmail.com>
2014-11-16 21:05:22 +0800

    在controller中调用service中的getById方法

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -28,22 +28,7 @@ public class ContactController {
     @RequestMapping("show")
     public String show(@RequestParam("id") Long id, Model model) {
         
-        model.addAttribute("contact", getById(id));
+        model.addAttribute("contact", contactService.getById(id));
         return "contact/show";
-    }
-    
-    public Contact getById(Long id) {
-        Contact contact = new Contact();
-        contact.setName("Shihang");
-        contact.setMobile("15235432994");
-        contact.setEmail("a@qq.com");
-        contact.setVpmn("652994");
-        contact.setHomeAddress("taiyuan");
-        contact.setOfficeAddress("taiyuan");
-        contact.setMemo("memo");
-        contact.setJob("HR");
-        contact.setJobLevel(4L);
-         
-        return contact;
     }
 }
--- src/main/java/com/baldurtech/contact/ContactService.java
+++ src/main/java/com/baldurtech/contact/ContactService.java
@@ -17,4 +17,19 @@ public class ContactService {
     public List<Contact> findAll() {
         return contactRepository.findAll();
     }
+    
+    public Contact getById(Long id) {
+        Contact contact = new Contact();
+        contact.setName("Shihang");
+        contact.setMobile("15235432994");
+        contact.setEmail("a@qq.com");
+        contact.setVpmn("652994");
+        contact.setHomeAddress("taiyuan");
+        contact.setOfficeAddress("taiyuan");
+        contact.setMemo("memo");
+        contact.setJob("HR");
+        contact.setJobLevel(4L);
+         
+        return contact;
+    }
 }
