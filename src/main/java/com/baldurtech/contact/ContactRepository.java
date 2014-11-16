9bdbcb972df7f954a1cfd290ad681b1832ade5c5
baijiayong <jiayongbai@gmail.com>
2014-11-17 03:14:13 +0800

    完成了更新会员的功能

--- src/main/java/com/baldurtech/contact/ContactRepository.java
+++ src/main/java/com/baldurtech/contact/ContactRepository.java
@@ -29,4 +29,8 @@ public class ContactRepository {
     public void save(Contact contact) {
         entityManager.persist(contact);
     }
+    
+    public Contact update(Contact contact) {
+        return entityManager.merge(contact);
+    }
 }
