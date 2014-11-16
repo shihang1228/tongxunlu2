394e182e8ceac248f8b3172877600d8954fcc6f5
baijiayong <jiayongbai@gmail.com>
2014-11-16 23:11:18 +0800

    添加了show页面的模板，并获将save方法改为post方法

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("contact")
@@ -19,20 +21,25 @@ public class ContactController {
         this.contactService = contactService;
     }
 
-    @RequestMapping("list")
+    @RequestMapping(value = "list")
     public String list(Model model) {
         model.addAttribute("contactList", contactService.findAll());
         return "contact/list";
     }
     
-    @RequestMapping("show")
+    @RequestMapping(value = "show")
     public String show(@RequestParam("id") Long id, Model model) {   
         model.addAttribute("contact", contactService.getById(id));
         return "contact/show";
     }
     
-    @RequestMapping("create")
+    @RequestMapping(value = "create")
     public String create() {
         return "contact/create";
+    }
+    
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String save(@ModelAttribute("contact") Contact contact) {
+        return "redirect:list";
     }
 }
