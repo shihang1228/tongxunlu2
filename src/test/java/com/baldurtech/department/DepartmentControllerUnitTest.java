8262b2b20ed77d205ded35c1f55a66c16f5e99a6
shihang <study1228.sh@gmail.com>
2014-11-19 12:01:21 +0800

    添加了departmentController的单元测试

--- src/test/java/com/baldurtech/department/DepartmentControllerUnitTest.java
+++ src/test/java/com/baldurtech/department/DepartmentControllerUnitTest.java
@@ -1,5 +1,41 @@
 package com.baldurtech.department;
 
+import java.util.List;
+import java.util.ArrayList;
+import org.springframework.ui.Model;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+ 
+import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
 public class DepartmentControllerUnitTest {
+    @Mock
+    Model model;
     
+    @Mock
+    DepartmentService departmentService;
+    
+    @InjectMocks
+    DepartmentController departmentController;
+    
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void 在list方法中当URL为department_list时应该访问list页面() {
+        List<Department> departmentList = new ArrayList<Department>();
+        
+        when(departmentService.list()).thenReturn(departmentList);
+        assertEquals("department/list", departmentController.list(model));
+        verify(departmentService).list();
+        verify(model).addAttribute("departmentList", departmentList);
+    }
 }
